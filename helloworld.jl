0a32a7723cb7f7d8dedf81f413048b7c84dde8b3
Antonio Dharmadas <antonio.louis@gmail.com>
2019-06-13 14:34:35 +0530

    corrected plot

--- helloworld.jl
+++ helloworld.jl
@@ -2,9 +2,19 @@
 using VegaDatasets, Queryverse, IndexedTables
 println("I am excited to learn Julia Queryverse")
 #create dataset from VegaDatasets samples
-
+cars = dataset("cars")
 #filter cars by column values
 cars |> @filter(_.Origin == "USA")
 
 #save filtered dataset to a file
 cars |> @filter(_.Origin == "USA") |> save("cars_usa.csv")
+
+#load data from a file
+load("cars_usa.csv")
+
+#plotting of datasets
+load("cars_usa.csv") |> @vlplot(:point, x=:Miles_per_Gallon, y=:weight_in_lbs, color="Cylinders:n")
+
+#saving and loading excel files
+cars |> save("cars_all.xls")
+load("cars_all.xls", index:1)
